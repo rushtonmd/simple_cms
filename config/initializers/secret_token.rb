9b670b77b5d3acb20cd80a7e908a7b20e745c6e3
Mark D Rushton <rushtonmd@gmail.com>
2012-09-09 15:16:37 -0700

    Initial merge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = '35188aefe8b69ac03abf822137c76e6446b28203ceb17c58e687f53c3b794b3495bb42f3bd196e74f64a0926a8af1f54fa9025008b3ea3ed072a83d86d6cad42'
