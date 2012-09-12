f288e58469df4c8f7060661aa3cc9278abdcc257
Mark D Rushton <rushtonmd@gmail.com>
2012-09-11 21:38:01 -0700

    Finished section 5

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,10 +1,15 @@
 class DemoController < ApplicationController
 
   def index
-  	render "hello"
+  	render "index"
   end
 
   def hello
+  	@testvariable = "shit ya'll"
+  	@array = [1,2,3,4,5]
+  	@id = params[:id].to_i
+  	@page = params[:page].to_i
+  	render "hello"
   end
 
 
